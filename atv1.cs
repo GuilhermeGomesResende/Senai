28405e657788450f9672c96eabace36ad5b6427b
Pedro Arthur <0001268862@senaimgaluno.com.br>
2026-06-25 16:27:26 -0300

    pedro arthur

---
+++ atv1.cs
@@ -0,0 +1,20 @@
+
+using System;
+class Atv1
+{
+    static void Main()
+    {
+        double s,p;
+        Console.WriteLine("Digite seu salário: ");
+        s = double.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o valor da prestação: ");
+        p = double.Parse(Console.ReadLine());
+        if ( p > s * 0.2)
+        {
+            Console.Write("O salário não pode ser concedido");
+        }
+        else
+        {
+            Console.Write("O salário pode ser concedido");
+        }
+        }}
