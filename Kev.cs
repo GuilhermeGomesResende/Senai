1283c62d389bb6f4e2f574e17566b204356dd97c
NyloOly <0001269698@senaimgaluno.com.br>
2026-06-30 14:40:43 -0300

    ok

--- Kev.cs
+++ Kev.cs
@@ -6,7 +6,6 @@ static void Main()
     int numero = 0;
         Console.Write("Digite um numero : ");
         numero = int.Parse(Console.ReadLine());
-        Console.Write ($"o sucessor do {numero} e {numero+1}");    
-        
+        Console.Write ($"o sucessor do {numero} e {numero+1}");
     }
 }
