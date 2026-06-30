bcdfc5a6b24ba49f71da2f0719c1386d4c5e39e6
NyloOly <0001269698@senaimgaluno.com.br>
2026-06-30 16:30:16 -0300

    Kev ATV

---
+++ Kevellyn/Exercicio5/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+static void Main()
+    {
+    double nota1 = 0;
+    double nota2 = 0;
+    double nota3 = 0;
+    Console.WriteLine("Qual será suas 3 notas? ");
+    nota1 = double.Parse(Console.ReadLine());
+    nota2 = double.Parse(Console.ReadLine());
+    nota3 = double.Parse(Console.ReadLine());
+    Console.WriteLine($"A media vai ser : {(nota1*2+nota2*3+nota3*5)/10:F2}");
+    }    
+}
