b729544781327ce6c025655f366c258f4c285fe4
GuilhermeGomesResende <0001271678@senaimgaluno.com.br>
2026-06-30 15:09:04 -0300

    Retirei arquivos desnecessarios

---
+++ Guilherme/Exercicio1/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program{
+    static void Main(){
+       int numero = 0;
+       Console.Write("digite seu numero: ");
+       numero = int.Parse(Console.ReadLine());
+       Console.Write ($"O sucessor de {numero} é {numero+1}");
+
+    }
+
+}
