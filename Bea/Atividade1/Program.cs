2e44aee746e40cbca1efc50150ae3122f7f19b92
beaazk <00012772139@senaimgaluno.com.br>
2026-07-01 16:17:00 -0300

    Atividades Bea

--- Bea/Atividade1/Program.cs
+++ Bea/Atividade1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program{
+      Static void main(){
+     int numero = 0;
+     Console.Write ("digite seu numero: ");
+     numero = int.Parse(Console.ReadLine()); 
+     Console.Write ($"o sucessor de {numero} e {numero+1}");
+
+      }
+
+}
--- Bea/Atividade2/Program.cs
+++ Bea/Atividade2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+    static void Main ()
+{
+    double nota1 = 0;
+    double nota2 = 0;
+    double nota3 = 0;
+    Console.WrideLine(Qual sera suas 3 notas?);
+    nota1 = double.Parse(Console.ReadLine());
+    nota2 = double.Parse(Console.ReadLine());
+    nota3 = double.Parse(Console.ReadLine());
+    Console.Write($"A media vai ser; {(nota1*2+nota2*3+nota3*5)/10:F2}");
+}
+
+}
