fba6d70185571ec202f58d233c80d0a26425711a
Pedro Arthur <0001268862@senaimgaluno.com.br>
2026-07-01 13:54:52 -0300

    atividade

--- Pedro/exercicio14/Program.cs
+++ Pedro/exercicio14/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        double i;
+        Console.WriteLine("Insira o índice de poluição : ");
+        i = double.Parse(Console.ReadLine());
+        if (i < 0.25)
+        {
+            Console.Write("O índice de poluição é aceitável.");
+        }
+        if (i >= 0.25 && i < 0.4)
+        {
+            Console.Write("Suspender grupo 1.");
+        }
+        if (i >= 0.4 && i < 0.5)
+        {
+            Console.Write("Suspender grupo 1 e 2.");
+        }
+        if (i > 0.5)
+        {
+            Console.Write("Suspender todos os grupos.");
+        }
+        else
+        {
+            Console.Write("Informação inválida");
+        }}}
+
+
+
